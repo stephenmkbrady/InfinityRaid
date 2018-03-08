34240c35cd819cc1cadc89f1bdc4b0d3c69f121d
bloop <stephen.mk.brady@gmail.com>
2018-03-08 16:35:28 +0000

    removed action button position checks, checking for name instead to allow for moving of nodes in scenetree. Changed how the timer affects the action buttons.

--- Scene/ActionButton.gd
+++ Scene/ActionButton.gd
@@ -12,33 +12,58 @@ func _ready():
 	game_state.connect("server_placed",self,"_on_server_placed")
 # make action 5 - 8 buttons grey and disabled
 	timer = get_parent().get_node("Power_Timer")
-	if self.get_position_in_parent() >= 5 and self.get_position_in_parent() <= 8:
+	if self.name == "Low" or self.name == "High" or self.name == "Medium":
 		self.set_disabled(true)
-
+		if self.name == "High" or self.name == "Medium":
+			self.set_visible(false)
+		
 func _pressed():
-# emit signal to tell other nodes what has been pressed
 	emit_signal("action_pressed", self.get_position_in_parent())
 
 func _process(delta):
 # monitor timer
-# enable and ungrey over time for buttons 5 - 8
 	var t = int(timer.time_left)
-	if t <= 1 and self.get_position_in_parent() == 5:
-		self.set_disabled(false)
-	if t <= turn_time / 3 and self.get_position_in_parent() == 6:
-		self.set_disabled(false)
-	if t <= turn_time / 2 and self.get_position_in_parent() == 7:
-		self.set_disabled(false)
-	if t <= turn_time - 10 and self.get_position_in_parent() == 8:
-		self.set_disabled(false)
-		
-# If timer is stopped:
-# set disable and grey action buttons 5 - 8 when a 5 - 8 has been placed on the hex
-	#
-	if timer.is_stopped():
-		if self.get_position_in_parent() >= 5 and self.get_position_in_parent() <= 8:
+	if t == turn_time / 3:
+		print("turn_time/3")
+		if self.get_name() == "High":
+			self.set_disabled(false)
+			self.set_visible(true)
+		elif self.get_name() == "Medium":
 			self.set_disabled(true)
+			self.set_visible(false)
+		elif self.get_name() == "Low":
+			self.set_disabled(true)
+			self.set_visible(false)
+	if t == turn_time / 2:
+		print("turn_time/3")
+		if self.get_name() == "High":
+			self.set_disabled(true)
+			self.set_visible(false)
+		elif self.get_name() == "Medium":
+			self.set_disabled(false)
+			self.set_visible(true)
+		elif self.get_name() == "Low":
+			self.set_disabled(true)
+			self.set_visible(false)
+	if t == turn_time - 10 :
+		print("turn_time-10")
+		if self.get_name() == "High":
+			self.set_disabled(true)
+			self.set_visible(false)
+		elif self.get_name() == "Medium":
+			self.set_disabled(true)
+			self.set_visible(false)
+		elif self.get_name() == "Low":
+			self.set_disabled(false)
+			self.set_visible(true)
 
+# If timer is stopped:
+	if timer.is_stopped():
+		if self.name == "Low" or self.name == "High" or self.name == "Medium":
+			self.set_disabled(true)
+			if self.name == "High" or self.name == "Medium":
+				self.set_visible(false)
+			
 func _on_computer_1_placed( ):
 	if self.get_position_in_parent() == 2:
 		self.set_disabled(true)
